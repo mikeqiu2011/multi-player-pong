3ae3f7eff8eb0f038675426d39f132fa3e67cfd6
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-18 19:40:40 +0800

    listen for client ready event on server

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ server.listen(PORT, () => {
     console.log('server is listening on', PORT);
 })
 
+let readyPlayerCount = 0
+
 io.on('connection', (socket) => {
     console.log('a user is connected...', socket.id);
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id);
+        readyPlayerCount++
+
+        if (readyPlayerCount == 2) {
+            //broadcast startgame
+
+        }
+    })
 })
